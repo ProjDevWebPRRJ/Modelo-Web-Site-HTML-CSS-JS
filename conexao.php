5374f39888f1ece40945a22007368acec2af85d6
Wellington Guilherme Rogério <wellingtonguilherme10@gmail.com>
2016-10-03 21:54:45 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?PHP
-$conecta = mysql_connect("mysql.hostinger.com.br", "u542430202_banco", "tads2016") or print (mysql_error()); 
+$conecta = mysql_connect("host.com.br", "usuario", "senha") or print (mysql_error()); 
 print "Conexão OK!"; 
 mysql_close($conecta); 
 ?>
